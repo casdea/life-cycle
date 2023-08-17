81a6042060f5066c3335c9810ce718f566cecc94
DESKTOP-KP43KU2\Gilbert Alexandre <gilbert.alexandre.eng@gmai.com>
2023-08-17 18:27:30 -0300

    Verificando eventos

--- src/app/check-exemplo/check-exemplo.component.ts
+++ src/app/check-exemplo/check-exemplo.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-check-exemplo',
   templateUrl: './check-exemplo.component.html',
   styleUrls: ['./check-exemplo.component.css']
 })
-export class CheckExemploComponent implements OnInit {
+export class CheckExemploComponent implements OnInit, DoCheck, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit {
 
-  constructor() { }
+  constructor() {
+    console.log("constructor");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked");
+  }
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit");
+  }
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked");
+  }
+  ngDoCheck(): void {
+    console.log("ngDoCheck");
+  }
 
   ngOnInit(): void {
+    console.log("ngOnInit");
+  }
+
+  adicionar() {
+    console.log("Adicionado");
+  }
+
+  remover() {
+    console.log("Removido");
   }
 
 }
